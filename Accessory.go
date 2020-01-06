6f0f19bf5b54a1c7c6eb50a3c94c56cbf1cd93e6
Gucan <415344598@qq.com>
2020-01-06 20:03:23 +0800

    Add files via upload

---
+++ Accessory.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/brutella/hc"
+	"github.com/brutella/hc/accessory"
+)
+
+func main() {
+
+	acc := accessory.NewSwitch(accessory.Info{Name:"Switch",Manufacturer:"Gucan",SerialNumber:"GCSWITCH",Model:"SI"})
+	acc.Switch.On.OnValueRemoteUpdate(func(on bool) { if on == true { print("a1 on\n") } else { print("a1 off\n") } })
+
+	config := hc.Config{Pin:"52037521",Port:"12345",StoragePath:"./db"}
+	t, err := hc.NewIPTransport(config, acc.Accessory)
+	if err != nil {
+		print(err)
+	}
+	t.Start()
+}
